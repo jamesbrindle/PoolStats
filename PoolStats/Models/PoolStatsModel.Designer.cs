d6afe26208b6cafa3f1eb8978f212d1d148d8234
Jamie Brindle <jamie.brindle7@gmail.com>
2023-01-21 16:56:16 +0000

    Migrated to Jamie-NAS

--- PoolStats/Models/PoolStatsModel.Designer.cs
+++ PoolStats/Models/PoolStatsModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'H:\Documents\Work\Repositories\PoolStats\PoolStats\Models\PoolStatsModel.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Repositories\PoolStats\PoolStats\Models\PoolStatsModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
